b672d60c81274c714f66168d966e4d957ea0f860
Ashif <114198892+Mhmd-Ashif@users.noreply.github.com>
2024-08-20 19:04:25 +0530

    fixed screen size

--- payit-user-app/components/Dashboard.tsx
+++ payit-user-app/components/Dashboard.tsx
@@ -16,99 +16,91 @@ export function Dashboard() {
   const [phone, setPhone] = useState<number>();
   const [password, setPassword] = useState<string>();
   const [loading, isLoading] = useState<boolean>(false);
+
   return (
-    <>
-      <div className="flex w-screen h-screen justify-center place-content-center gap-10 md:ml-8">
-        <div className="flex items-center  py-12 w-7/12">
-          <div className="mx-auto grid w-[350px] gap-6 ">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Login</h1>
-              <p className="text-balance text-muted-foreground">
-                Enter your phone number below to login to your account
-              </p>
-            </div>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
+    <div className="flex flex-col lg:flex-row w-full h-screen justify-center items-center lg:gap-10">
+      <div className="flex items-center py-12 w-full lg:w-1/2">
+        <div className="mx-auto w-full max-w-md p-6 lg:p-0">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-muted-foreground mt-2">
+              Enter your phone number below to login to your account
+            </p>
+          </div>
+          <div className="mt-6">
+            <div className="space-y-4">
+              <div>
                 <Label htmlFor="phone">Phone Number</Label>
                 <Input
                   id="phone"
                   type="text"
                   maxLength={10}
                   placeholder="86105*****"
-                  className="rounded-md p-4 border"
+                  className="mt-2 p-4 border rounded-md w-full"
                   required
-                  onChange={(e) => {
-                    setPhone(Number(e.target.value));
-                  }}
+                  onChange={(e) => setPhone(Number(e.target.value))}
                 />
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
+              <div>
+                <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   type="password"
                   required
-                  className="rounded-md p-4 border"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
+                  className="mt-2 p-4 border rounded-md w-full"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <hr></hr>
-              <Button
-                disabled={loading}
-                type="submit"
-                className="w-full  p-4  font-semibold text-md"
-                onClick={async () => {
-                  isLoading(true);
-                  const res = await signIn("credentials", {
-                    phone: phone,
-                    password: password,
-                    redirect: false,
+            </div>
+            <hr className="my-6" />
+            <Button
+              disabled={loading}
+              type="submit"
+              className="w-full p-4 font-semibold text-md"
+              onClick={async () => {
+                isLoading(true);
+                const res = await signIn("credentials", {
+                  phone: phone,
+                  password: password,
+                  redirect: false,
+                });
+                if (res?.error == null) {
+                  router.push("/dashboard");
+                } else {
+                  isLoading(false);
+                  toast({
+                    variant: "destructive",
+                    title: "User Doesn't Exist With The Given Credentials",
+                    description: "Please Enter Valid Phone Number and Password",
                   });
-                  if (res?.error == null) {
-                    router.push("/dashboard");
-                  } else {
-                    isLoading(false);
-                    toast({
-                      variant: "destructive",
-                      title: "User Doesn't Exist With The Given Credentials",
-                      description:
-                        "Please Enter Valid Phone Number and Password",
-                    });
-                  }
-                }}
-              >
-                {loading ? (
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                ) : (
-                  ""
-                )}
-                {loading ? "Logging In " : "Login"}
-              </Button>
-            </div>
-            <div className="mt-2 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="/signup" className="underline">
-                Sign up
-              </a>
-            </div>
+                }
+              }}
+            >
+              {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : ""}
+              {loading ? "Logging In " : "Login"}
+            </Button>
           </div>
-        </div>
-        <div className="hidden bg-muted lg:block">
-          <div className="font-mono text-5xl font-bold text-white absolute  text-center top-32">
-            Payment Done Easier with payit
-            <ul className=" text-lg mt-8">
-              <li>- Transaction Done Via Phone Number</li>
-              <li>- Quick and Reliable P2P transfers</li>
-              <li>- Payments can be Visualtized by using `PAYIT`</li>
-            </ul>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <a href="/signup" className="underline">
+              Sign up
+            </a>
           </div>
-          <img src={world.src} className="fit"></img>
         </div>
       </div>
-    </>
+      <div className="hidden lg:flex lg:w-1/2 h-full bg-muted relative">
+        <div className="absolute top-1/4 left-0 right-0 text-center text-white p-8">
+          <h2 className="font-mono text-2xl lg:text-5xl font-bold">
+            Payment Done Easier with payit
+          </h2>
+          <ul className="text-sm lg:text-lg mt-4 space-y-2">
+            <li>- Transaction Done Via Phone Number</li>
+            <li>- Quick and Reliable P2P transfers</li>
+            <li>- Payments can be Visualized by using `PAYIT`</li>
+          </ul>
+        </div>
+        <img src={world.src} className="w-full h-full object-fill" />
+      </div>
+    </div>
   );
 }
